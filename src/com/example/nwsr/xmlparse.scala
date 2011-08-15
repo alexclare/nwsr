18276a9e17d62e5da130e92428263006d0d4c7dd
Alexander Clare <alexander.clare@gmail.com>
2011-08-14 21:29:33 -0500

    Working XML parser for RSS
    Uses the Android built-in SAX parser; doesn't work for Atom, exceptions
    are a bit iffy

--- src/com/example/nwsr/xmlparse.scala
+++ src/com/example/nwsr/xmlparse.scala
@@ -1,53 +1,95 @@
 package com.example.nwsr
 
-import scala.xml.Elem
-import scala.xml.Node
-import scala.xml.NodeSeq
-import scala.xml.XML
+import android.sax.RootElement
+import android.sax.EndTextElementListener
+import android.sax.StartElementListener
+import android.util.Xml
 
 import java.io.InputStream
+
+import org.xml.sax.Attributes
+import org.xml.sax.SAXException
 
 // Etag/last-modified in HTTP header
 // set "updated" based on user refresh, no date parsing
 
 class NotFeedException() extends Exception() { }
 
-class FeedData (
-  val title: String,
-  val link: String
-)
-{
+class FeedData {
+  var title: String = _
+  var link: String = _
 }
 
-// Could be written really easily in Scala, but there's a bug in Android
-//   (involving SAXNotSupportedException) that wasn't fixed until 2.2
-// Need handling for malformed RSS/Atom
+/* Could be written really easily in Scala, but there's a bug in Android
+   involving the SAX parser it depends on that wasn't fixed until 2.2
+
+   Using the android.sax interface instead
+*/
+
+// Need handling for malformed RSS/Atom, goes near the "xml.parse" bit
 object FeedData {
-  def parseRSS(channel: Node): FeedData = {
+  def parseRSS(input: InputStream): FeedData = {
+    val result = new FeedData()
+    val root = new RootElement("rss")
+    val channel = root.getChild("channel")
+    channel.getChild("title").setEndTextElementListener(
+      new EndTextElementListener() {
+        def end(body: String) {
+          result.title = body
+        }
+    })
+    channel.getChild("link").setEndTextElementListener(
+      new EndTextElementListener() {
+        def end(body: String) {
+          result.link = body
+        }
+    })
+    //val story = channel.getChild("item")
     /*
     val stories = for (story <- channel \ "item")
                   yield Story((story \ "title").text, (story \ "link").text)
     */
-    new FeedData((channel \ "title").text,
-                 (channel \ "link").text)
+    Xml.parse(input, Xml.Encoding.UTF_8, root.getContentHandler())
+    result
   }
 
-  def parseAtom(feed: Elem, nodes: NodeSeq): FeedData = {
+  def parseAtom(input: InputStream): FeedData = {
+    val result = new FeedData()
+    val root = new RootElement("feed")
+    root.getChild("title").setEndTextElementListener(
+      new EndTextElementListener() {
+        def end(body: String) {
+          result.title = body
+        }
+    })
+    root.getChild("link").setStartElementListener(
+      new StartElementListener() {
+        def start(attrs: Attributes) {
+          // be careful here - might not have the right value in qName, in
+          //   which case I'd have to use another getValue function
+          result.link = attrs.getValue("href")
+        }
+    })
     /*
     val stories = for (story @ <entry>{_*}</entry> <- nodes)
                   yield Story((story \ "title").text,
                               ((story \ "link").head \ "@href").text)
     */
-    new FeedData((feed \ "title").text,
-                 ((feed \ "link").head \ "@href").text)
+    Xml.parse(input, Xml.Encoding.UTF_8, root.getContentHandler())
+    result
   }
 
   def parseFeed(input: InputStream): FeedData = {
-    val document = XML.load(input)
-    document match {
-      case <rss>{channel}</rss> => parseRSS(channel)
-      case <feed>{inner @ _*}</feed> => parseAtom(document, inner)
-      case _ => throw new NotFeedException()
+    var result: Option[FeedData] = None
+    try {
+      result = Some(parseRSS(input))
+    } catch {
+      // doesn't work; Expat exception instead
+      case ex: SAXException => result = Some(parseAtom(input))
+    }
+    result match {
+      case Some(data) => data
+      case None => throw new SAXException()
     }
   }
 }
